fea0b2ffe1a47233cede9836638bce997a0c0a77
xiaowei <xiaowei20150509YIUHLK>
2021-07-02 11:36:36 +0800

    删除工程文件

--- QtCefView/CefBrowser.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <QtWidgets/QApplication>
-#include <QtWidgets/QMainWindow>
-#include <QTimer>
-#include "QCefWebView.h"
-QCEFWEBVIEWSHARED_EXPORT int QCefProcessStart();
-QCEFWEBVIEWSHARED_EXPORT void QCefMessageLoop();
-QCEFWEBVIEWSHARED_EXPORT void QCefProcessExit();
